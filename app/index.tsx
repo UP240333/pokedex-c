19760bd3e9ea8449384fd37b405bc4debb777b72
Diego Carrillo <up240333@alumnos.upa.edu.mx>
2026-03-02 15:31:57 -0600

    add getPokemons fetch

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,26 @@
+import { useEffect } from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemons();
+  }, []);
+
+  const getPokemons = async () => {
+    try {
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+      const response = await fetch(URL, { method: "GET" }); //Pide info  await:Espera a que te mande solo
+      if (response.ok) {
+        console.log("Request Ok");
+      } else {
+        console.log("Bad Request");
+      }
+    } catch (error) {
+      console.log("Ocurrio un error");
+    }
+  };
+
   return (
     <View
       style={{
